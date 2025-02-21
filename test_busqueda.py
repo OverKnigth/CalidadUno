42fa2245f87433fea7332db3650db49a5c892299
OverKnigth <stalin.m0012@gmail.com>
2025-02-21 09:11:37 -0500

    Subir

--- test_busqueda.py
+++ test_busqueda.py
@@ -3,8 +3,8 @@ from busqueda import busqueda_lineal, busqueda_binaria_iterativa, busqueda_binar
 
 @pytest.mark.parametrize("arr, x, esperado", [
     ([64, 34, 25, 12, 22, 11, 90], 22, 4),
-    ([10, 20, 30, 40, 50], 30, 2),
-    ([5, 1, 7, 9, 3], 9, 3),
+    ([50, 30, 10, 70, 20], 10, 2),
+    ([5, 3, 8, 1, 6], 3, 1),
     ([1, 2, 3, 4, 5], 6, -1)  
 ])
 def test_busqueda_lineal(arr, x, esperado):
@@ -12,15 +12,17 @@ def test_busqueda_lineal(arr, x, esperado):
 
 @pytest.mark.parametrize("arr, x, esperado", [
     ([11, 12, 22, 25, 34, 64, 90], 22, 2),
-    ([1, 3, 5, 7, 9], 7, 3),
-    ([10, 20, 30, 40, 50], 25, -1)  
+    ([2, 4, 6, 8, 10], 6, 2),
+    ([3, 5, 7, 9, 11], 8, -1),
+    ([1, 3, 5, 7, 9], 7, 3)  
 ])
 def test_busqueda_binaria_iterativa(arr, x, esperado):
     assert busqueda_binaria_iterativa(arr, x) == esperado
 
 @pytest.mark.parametrize("arr, x, esperado", [
     ([11, 12, 22, 25, 34, 64, 90], 22, 2),
-    ([1, 3, 5, 7, 9], 9, 4),
+    ([3, 5, 7, 9, 11], 5, 1),
+    ([20, 40, 60, 80, 100], 60, 2),
     ([10, 20, 30, 40, 50], 60, -1)  
 ])
 def test_busqueda_binaria_recursiva(arr, x, esperado):
--- test_ordenamiento.py
+++ test_ordenamiento.py
@@ -3,42 +3,33 @@ from ordenamiento import bubble_sort, seleccion_sort, insertion_sort
 
 
 def test_bubble_sort():
-    # Caso 1: Lista normal
-    arr = [64, 34, 25, 12, 22, 11, 90]
-    assert bubble_sort(arr[:]) == [11, 12, 22, 25, 34, 64, 90]
+    arr = [987654, 123456, 654321, 111222, 999999]
+    assert bubble_sort(arr[:]) == [111222, 123456, 654321, 987654, 999999]
 
-    # Caso 2: Lista en orden inverso
-    arr = [5, 4, 3, 2, 1]
-    assert bubble_sort(arr[:]) == [1, 2, 3, 4, 5]
+    arr = [45, -12, 78, -5, 23, -9]
+    assert bubble_sort(arr[:]) == [-12, -9, -5, 23, 45, 78]
 
-    # Caso 3: Lista con elementos repetidos
-    arr = [3, 1, 2, 3, 1, 2]
-    assert bubble_sort(arr[:]) == [1, 1, 2, 2, 3, 3]
+    arr = [3.14, 1.41, 2.71, 0.99, 4.56]
+    assert bubble_sort(arr[:]) == [0.99, 1.41, 2.71, 3.14, 4.56]
 
 
 def test_seleccion_sort():
-    # Caso 1: Lista normal
-    arr = [64, 34, 25, 12, 22, 11, 90]
-    assert seleccion_sort(arr[:]) == [11, 12, 22, 25, 34, 64, 90]
+    arr = [-10, -5, -20, 0, 10]
+    assert seleccion_sort(arr[:]) == [-20, -10, -5, 0, 10]
 
-    # Caso 2: Lista vacía
-    arr = []
-    assert seleccion_sort(arr[:]) == []
+    arr = [1000000000, 999999999, 1000000001, 1000000002]
+    assert seleccion_sort(arr[:]) == [999999999, 1000000000, 1000000001, 1000000002]
 
-    # Caso 3: Lista con un solo elemento
-    arr = [5]
-    assert seleccion_sort(arr[:]) == [5]
+    arr = [50, 40, 30, 20, 10]
+    assert seleccion_sort(arr[:]) == [10, 20, 30, 40, 50]
 
 
 def test_insertion_sort():
-    # Caso 1: Lista ya ordenada
-    arr = [1, 2, 3, 4, 5]
-    assert insertion_sort(arr[:]) == [1, 2, 3, 4, 5]
+    arr = [-50, 20, -30, 10, -10]
+    assert insertion_sort(arr[:]) == [-50, -30, -10, 10, 20]
 
-    # Caso 2: Lista en orden inverso
-    arr = [5, 4, 3, 2, 1]
-    assert insertion_sort(arr[:]) == [1, 2, 3, 4, 5]
+    arr = [0, 0, 0, 0, 0]
+    assert insertion_sort(arr[:]) == [0, 0, 0, 0, 0]
 
-    # Caso 3: Lista con números negativos
-    arr = [3, -1, 2, -3, 1, 0]
-    assert insertion_sort(arr[:]) == [-3, -1, 0, 1, 2, 3]
+    arr = [3, 2.5, 5, 1.7, 4]
+    assert insertion_sort(arr[:]) == [1.7, 2.5, 3, 4, 5]
